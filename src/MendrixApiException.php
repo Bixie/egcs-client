809b31e35b181c299147d4b0ce0282988e57469c
Matthijs Alles <matthijs@bixie.nl>
2019-07-12 10:35:33 +0200

    get response from exception

--- src/MendrixApiException.php
+++ src/MendrixApiException.php
@@ -4,7 +4,26 @@
 namespace Egcs;
 
 
+use GuzzleHttp\Exception\ServerException;
+use GuzzleHttp\Psr7\Response;
+
 class MendrixApiException extends \Exception
 {
+
+    public function hasResponse (): bool
+    {
+        return $prev = $this->getPrevious() and $prev instanceof ServerException and $prev->hasResponse();
+    }
+
+    public function getResponse (): ?Response
+    {
+        return $this->hasResponse() ? $this->getPrevious()->getResponse() : null;
+    }
+
+    public function getResponseData (): ?array
+    {
+        $body = (string)$this->getResponse()->getBody();
+        return json_decode($body, true);
+    }
 
 }
